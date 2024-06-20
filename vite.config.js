340858f9ba9ab96439f7d9628ef4b5cc067d6ac5
ANANT TIWARY <143578737+annattiwary12@users.noreply.github.com>
2024-06-20 21:06:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/WebPage/', // Adjust this according to your GitHub Pages setup
+  build: {
+    outDir: 'docs' // Adjust the output directory to 'docs' for GitHub Pages
+  }
+});
